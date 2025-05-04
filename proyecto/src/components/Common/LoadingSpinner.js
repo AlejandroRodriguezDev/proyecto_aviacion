6aa5b2840c8d5e423af02ed8d806007683855a10
AlejandroRodriguezDev <alejandro_rod.c@uao.edu.co>
2025-05-03 20:57:50 -0500

    quitar lo que no sirve

--- proyecto/src/components/Common/LoadingSpinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './LoadingSpinner.module.css';
-
-const LoadingSpinner = ({ size = '40px' }) => {
-  return <div className={styles.spinner} style={{ width: size, height: size }}></div>;
-};
-
-export default LoadingSpinner;
